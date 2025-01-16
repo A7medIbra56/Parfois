f15d78f7c8b3ced22dae2cdecb3b1383d7936cb9
Ahmed <ahmed@example.com>
2025-01-16 21:53:29 +0200

    update react jason

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tsconfingPath from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tsconfingPath()],
+  build:{
+    sourcemap: true,
+  }
 })
