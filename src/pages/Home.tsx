a6055033dff1145ea1b2b067943eebbb734f0f34
Ahmed <ahmed@example.com>
2025-01-20 19:30:42 +0200

    update react jason

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,31 +4,6 @@ export default function Home() {
   return (
     <div>
      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
-        <Card.Text>
-          This is a simple card example using React-Bootstrap. It contains an image, title, text, and a button.
-        </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </div>
